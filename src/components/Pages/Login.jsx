b217e4a7d1a32d012a39cbc1c54b7d0b4db7d8aa
bhaskar3009 <bhaskarchavan2001@gmail.com>
2022-10-30 18:03:06 +0530

    landingpage

--- src/components/Pages/Login.jsx
+++ src/components/Pages/Login.jsx
@@ -8,15 +8,16 @@ class Login extends React.Component{
         return(
             <section id="main">
                     <div className="nav-item">
-                        <a className="navbar-brand" href="/">Vusic</a>
+                        <a className="navbar-brand" href="/">MUSIC-PHI</a>
                     </div>
                     <div className="main-row">
                         <div className="main-row-img">
                             <img className="head-phone-img" src={HeadPhone} alt=""/>
                         </div>
                         <div className="main-row-text">
-                            <h1>Music for everyone</h1>
-                            <p>Without music, life would be a mistake</p>
+                            <h1>MUSIC-PHI</h1>
+                            <h1>PLAY-HEAL-INTERAC</h1>
+                            <p>When Words Fail, Music Speaks</p>
                             <Link to={"/home"} className="btn">
                                 Start Listening
                             </Link>
