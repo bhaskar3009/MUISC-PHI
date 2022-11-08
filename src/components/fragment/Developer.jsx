6086f81dfb5bef958173fd2b3f5926ee24ee93c3
bhaskar3009 <bhaskarchavan2001@gmail.com>
2022-11-08 17:46:28 +0530

    about updated

--- src/components/fragment/Developer.jsx
+++ src/components/fragment/Developer.jsx
@@ -7,7 +7,8 @@ import {Facebook, Instagram, LinkedIn, Portrait, Twitter} from "@material-ui/ico
 const Developer = () => {
     return (
         <div className={"Developer"}>
-            <h3 className={"Developer-head"}>Meet the developer</h3>
+            <h3 className={"Developer-head"}>Meet the developers</h3>
+            <br />
             <div className="Developer-profile">
                 <div className="Developer-profileCard">
                     <img src={AvatarImage} alt="Profile"/>
@@ -20,29 +21,154 @@ const Developer = () => {
                 <div className="Developer-profileDetails">
                     <p>A Computer Engineering Student at KJSIEIT, Sion.</p>
                     <p>Graduating in 2024 and looking for a responsible position to gain practical knowledge</p>
-                    <p>A front-endd web developer and a Competitive coder.</p>
+                    <p>A front-end web developer and a Competitive coder.</p>
                     <p>I love designing fully responsive websites.</p>
                     <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
                     <div className="Card-btn">
-                        <IconButton target={"_blank"}  href={"https://www.facebook.com/vishal.kld"} title={"vishal.kld"}>
+                        <IconButton target={"_blank"}  href={"#"} title={"vishal.kld"}>
+                        {/* https://www.facebook.com/vishal.kld */}
                             <Facebook/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://twitter.com/Vishal_kld"}  title={"Vishal_kld"}>
+                        <IconButton target={"_blank"} href={"#"}  title={"Vishal_kld"}>
+                        {/* https://twitter.com/Vishal_kld */}
                             <Twitter/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.linkedin.com/in/vishalsingh-/"}  title={"vishalsingh-"}>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh-"}>
+                        {/* https://www.linkedin.com/in/vishalsingh-/ */}
                             <LinkedIn/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://www.instagram.com/vishalsingh023/"}  title={"vishalsingh023"}>
+                        <IconButton target={"_blank"} href={"https://www.instagram.com/itz_bhaskar_here/"}  title={"vishalsingh023"}>
                             <Instagram/>
                         </IconButton>
-                        <IconButton target={"_blank"} href={"https://vishal.thetechnician.in/"}  title={"Web Portfolio"}>
+                        <IconButton target={"_blank"} href={"#"}  title={"Web Portfolio"}>
+                        {/* https://vishal.thetechnician.in/ */}
                             <Portrait/>
                         </IconButton>
                     </div>
                 </div>
 
             </div>
+            <br />
+            <div className="Developer-profile">
+                <div className="Developer-profileCard">
+                    <img src={AvatarImage} alt="Profile"/>
+                    <div className={"Card-details"}>
+                        <h3>Mahendra Bhanushali</h3>
+                        <p>Python Developer</p>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="Developer-profileDetails">
+                    <p>A Computer Engineering Student at KJSIEIT, Sion.</p>
+                    <p>Graduating in 2024 and looking for a responsible position to gain practical knowledge</p>
+                    <p>A python and c programmer and photoshop editor.</p>
+                    <p>I love designing fully responsive websites.</p>
+                    <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
+                    <div className="Card-btn">
+                        <IconButton target={"_blank"}  href={"#"} title={"vishal.kld"}>
+                        {/* https://www.facebook.com/vishal.kld */}
+                            <Facebook/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"Vishal_kld"}>
+                        {/* https://twitter.com/Vishal_kld */}
+                            <Twitter/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh-"}>
+                        {/* https://www.linkedin.com/in/vishalsingh-/ */}
+                            <LinkedIn/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh023"}>
+                            <Instagram/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"Web Portfolio"}>
+                        {/* https://vishal.thetechnician.in/ */}
+                            <Portrait/>
+                        </IconButton>
+                    </div>
+                </div>
+
+            </div>
+            <br />
+            <div className="Developer-profile">
+                <div className="Developer-profileCard">
+                    <img src={AvatarImage} alt="Profile"/>
+                    <div className={"Card-details"}>
+                        <h3>Deevkumar Bharuka</h3>
+                        <p>Back-End developer</p>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="Developer-profileDetails">
+                    <p>A Computer Engineering Student at KJSIEIT, Sion.</p>
+                    <p>Graduating in 2024 and looking for a responsible position to gain practical knowledge</p>
+                    <p>A back-end web developer.</p>
+                    <p>I love to make website live and dynamic by backend.</p>
+                    <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
+                    <div className="Card-btn">
+                        <IconButton target={"_blank"}  href={"#"} title={"vishal.kld"}>
+                        {/* https://www.facebook.com/vishal.kld */}
+                            <Facebook/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"Vishal_kld"}>
+                        {/* https://twitter.com/Vishal_kld */}
+                            <Twitter/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh-"}>
+                        {/* https://www.linkedin.com/in/vishalsingh-/ */}
+                            <LinkedIn/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh023"}>
+                            <Instagram/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"Web Portfolio"}>
+                        {/* https://vishal.thetechnician.in/ */}
+                            <Portrait/>
+                        </IconButton>
+                    </div>
+                </div>
+
+            </div>
+            <br />
+            <div className="Developer-profile">
+                <div className="Developer-profileCard">
+                    <img src={AvatarImage} alt="Profile"/>
+                    <div className={"Card-details"}>
+                        <h3>Naina Dogra</h3>
+                        <p>Front-End developer</p>
+                        <p></p>
+                    </div>
+                </div>
+                <div className="Developer-profileDetails">
+                    <p>A Computer Engineering Student at KJSIEIT, Sion.</p>
+                    <p>Graduating in 2024 and looking for a responsible position to gain practical knowledge</p>
+                    <p>A front-end web developer and a Competitive coder.</p>
+                    <p>I love designing fully responsive websites.</p>
+                    <p>I have a keen interest in developing projects, whenever I want to learn something new.</p>
+                    <div className="Card-btn">
+                        <IconButton target={"_blank"}  href={"#"} title={"vishal.kld"}>
+                        {/* https://www.facebook.com/vishal.kld */}
+                            <Facebook/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"Vishal_kld"}>
+                        {/* https://twitter.com/Vishal_kld */}
+                            <Twitter/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh-"}>
+                        {/* https://www.linkedin.com/in/vishalsingh-/ */}
+                            <LinkedIn/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"vishalsingh023"}>
+                            <Instagram/>
+                        </IconButton>
+                        <IconButton target={"_blank"} href={"#"}  title={"Web Portfolio"}>
+                        {/* https://vishal.thetechnician.in/ */}
+                            <Portrait/>
+                        </IconButton>
+                    </div>
+                </div>
+
+            </div>
+            <br />
         </div>
     );
 }
